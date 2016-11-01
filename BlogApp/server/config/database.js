68ef990f6883121ac06445b7e0a0e1d36f4be564
tarlit <tarlitan@gmail.com>
2016-11-01 19:59:46 +0200

    Start project

---
+++ BlogApp/server/config/database.js
@@ -0,0 +1,22 @@
+let mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+module.exports = (config) => {
+    mongoose.connect(config.db)
+
+    let db = mongoose.connection
+
+    db.once('open', (err) => {
+        if (err) {
+            console.log(err)
+                // throw err
+        }
+
+        console.log('MongoDB ready!')
+    })
+
+    db.on('error', (err) => {
+        console.log('Databese error: ' + err)
+    })
+}
---
+++ BlogApp/server/config/routes.js
@@ -0,0 +1,12 @@
+let controllers = require('../controllers')
+
+module.exports = (app) => {
+    app.get('/', controllers.home.index)
+    app.get('/about', controllers.home.about)
+
+    app.all('*', (req, res) => {
+        res.status(404)
+        res.send('Not Found!')
+        res.end()
+    })
+}
