08bbad83f38a16bcb47e463be1b9595a84d4ad81
tarlit <tarlitan@gmail.com>
2016-11-02 16:09:05 +0200

    Register form added

--- BlogApp/server/config/routes.js
+++ BlogApp/server/config/routes.js
@@ -5,6 +5,7 @@ module.exports = (app) => {
     app.get('/about', controllers.home.about)
 
     app.get('/users/register', controllers.users.register)
+    app.post('/users/create', controllers.users.create)
 
     app.all('*', (req, res) => {
         res.status(404)
--- BlogApp/server/controllers/users-controller.js
+++ BlogApp/server/controllers/users-controller.js
@@ -1,5 +1,18 @@
+let encryption = require('../utilities/encryption')
+
 module.exports = {
     register: (req, res) => {
         res.render('users/register')
+    },
+    create: (req, res) => {
+        let user = req.body
+
+        if (user.password !== user.confirmPassword) {
+            res.render('users/register', { globalError: 'Password does not match!' })
+        } else {
+            user.salt = encryption.generateSalt()
+            user.hashedPassword = encryption.generateHashedPassword(user.salt, user.password)
+            res.send('It Works!')
+        }
     }
 }
